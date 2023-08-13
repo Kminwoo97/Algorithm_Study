570ec20fe1e6c99f41308e5d444fe982483da455
BakChanGyu <qmrma987@gmail.com>
2023-08-13 19:21:16 +0900

    lv2 점프와 순간 이동

---
+++ programmers/chan/level2/점프와_순간_이동.java
@@ -0,0 +1,15 @@
+public class 점프와_순간_이동 {
+    public int solution(int n) {
+        int ans = 0;
+
+        while (n > 0) {
+            if (n % 2 != 0) {
+                ans++;
+            }
+
+            n /= 2;
+        }
+
+        return ans;
+    }
+}
