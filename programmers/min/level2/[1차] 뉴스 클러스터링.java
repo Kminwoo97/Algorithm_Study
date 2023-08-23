fb1970546bb3974144a0e8106acef0ca1e4a9ecb
Kminwoo97 <92236489+Kminwoo97@users.noreply.github.com>
2023-08-23 15:14:10 +0900

    프로그래머스 lv2 [1차] 뉴스 클러스터링

--- programmers/min/level2/[1차] 뉴스 클러스터링.java
+++ programmers/min/level2/[1차] 뉴스 클러스터링.java
@@ -1,59 +1,54 @@
 import java.util.*;
 
-
 class Solution {
     public int solution(String str1, String str2) {
         int answer = 0;
         
-        //대문자로 치환
-        str1 = str1.toUpperCase();
-        str2 = str2.toUpperCase();
+        //입력값 소문자로
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
         
-        //str1, str2 각각에 대해 다중집합 만들기
-        Map<String, Integer> map1 = new HashMap<>();
-        Map<String, Integer> map2 = new HashMap<>();
-        for(int i=0; i<str1.length() - 1; i++){
-            String key = str1.substring(i, i+2).replaceAll("[^A-Z]", "");
-            if(key.length() == 2){
-                map1.put(key, map1.getOrDefault(key, 0) + 1);
-            }
+        //다중집합 만들기
+        Map<String, Integer> mapA = new HashMap<>();
+        Map<String, Integer> mapB = new HashMap<>();
+        for(int i=0; i<str1.length()-1; i++){
+            String key = str1.substring(i,i+2).replaceAll("[^a-z]","");
+            if(key.length() != 2)
+                continue;
+            mapA.put(key, mapA.getOrDefault(key, 0) + 1);
         }
-        for(int i=0; i<str2.length() - 1; i++){
-            String key = str2.substring(i, i+2).replaceAll("[^A-Z]", "");
-            if(key.length() == 2){
-                map2.put(key, map2.getOrDefault(key, 0) + 1);
-            }
+        for(int i=0; i<str2.length()-1; i++){
+            String key = str2.substring(i,i+2).replaceAll("[^a-z]","");
+            if(key.length() != 2)
+                continue;
+            mapB.put(key, mapB.getOrDefault(key, 0) + 1);
         }
         
-        double hap = 0;
-        double gyo = 0;
-        for(String key : map1.keySet()){
-            if(map2.containsKey(key)){
-                hap += Math.max(map1.get(key), map2.get(key));
-                gyo += Math.min(map1.get(key), map2.get(key));
+        //교집합, 합집합 구하기
+        int intersect = 0;
+        int union = 0;
+        for(String key : mapA.keySet()){
+            if(mapB.containsKey(key)){
+                intersect += Math.min(mapA.get(key), mapB.get(key));
+                union += Math.max(mapA.get(key), mapB.get(key));
                 
-                map2.put(key,0);
+                mapB.remove(key);
             }else{
-                hap += map1.get(key);
+                union += mapA.get(key);
             }
         }
         
-        //남은 것들 더하기
-        for(String key : map2.keySet())
-            hap += map2.get(key);
+        for(String key : mapB.keySet()){
+            union += mapB.get(key);
+        }
         
-        
-        if(map1.isEmpty() && map2.isEmpty())
+        //정답 도출
+        if(intersect == 0 && union == 0)
             answer = 65536;
-        else
-            answer = (int)((gyo / hap) * 65536);
-    
+        else{
+            answer = (int)((intersect / (double)union) * 65536);
+        }
         
         return answer;
-    }
-    
-    public double Jkad(ArrayList<String> str1, ArrayList<String> str2){
-        
-        return 1.0;
     }
 }
