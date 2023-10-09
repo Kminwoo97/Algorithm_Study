32091e40de467bfb8fb10d3e1d5abe167fb8912c
BakChanGyu <103313634+BakChanGyu@users.noreply.github.com>
2023-10-09 23:10:31 +0900

    lv2 큰 수 만들기

---
+++ programmers/chan/level2/큰_수_만들기.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String solution(String number, int k) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        
+        int index = 0;
+        
+        for (int i = 0; i < number.length() - k; i++) {
+            char max = '0';
+            
+            for (int j = index; j <= k + i; j++) {
+                if (max < number.charAt(j)) {
+                    max = number.charAt(j);
+                    index = j + 1;
+                }
+            }
+            
+            sb.append(max);
+        }
+
+        return answer = sb.toString();
+    }
+}
