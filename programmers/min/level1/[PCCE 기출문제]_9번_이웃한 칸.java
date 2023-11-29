2097516150d91a6b2ef6de9c9f5e3538a3602793
Kminwoo97 <92236489+Kminwoo97@users.noreply.github.com>
2023-11-29 18:35:33 +0900

    프로그래머스 lv1 [PCCE 기출문제]_9번_이웃한 칸

---
+++ programmers/min/level1/[PCCE 기출문제]_9번_이웃한 칸.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(String[][] board, int h, int w) {
+        int answer = 0;
+        
+        int[] dx = {0,0,1,-1};
+        int[] dy = {1,-1,0,0};
+        
+        String color = board[h][w];
+        for(int i=0; i<4; i++){
+            int nh = h + dx[i];
+            int nw = w + dy[i];
+            
+            if(nh < 0 || nh >= board.length || nw < 0 || nw >= board[0].length)
+                continue;
+            
+            if(board[nh][nw].equals(color))
+                answer++;
+        }
+        
+        return answer;
+    }
+}
