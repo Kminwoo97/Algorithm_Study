5f1948be6ccb4c9c38274cc391d5dc66d7f0296a
BakChanGyu <103313634+BakChanGyu@users.noreply.github.com>
2023-12-10 16:24:02 +0900

    lv3 최적의 행렬 곱셈(2)

--- programmers/chan/level3/최적의_행렬_곱셈.java
+++ programmers/chan/level3/최적의_행렬_곱셈.java
@@ -3,50 +3,49 @@ import java.util.*;
 class Solution {
     public int solution(int[][] matrix_sizes) {
         int answer = 0;
-        Queue<int[]> pq = new LinkedList<>();
         List<int[]> list = new ArrayList<>();
-        int[] min = new int[]{-1, Integer.MAX_VALUE};
         
-        for (int i = 1; i < matrix_sizes.length; i++) {
-            list.add(matrix_sizes[i - 1]);
-            
-            int multiValue = matrix_sizes[i - 1][0] * matrix_sizes[i][1];
-            
-            if (min[1] > multiValue) {
-                min[0] = i - 1;
-                min[1] = multiValue;
-            }
+        for (int i = 0; i < matrix_sizes.length; i++) {
+            list.add(matrix_sizes[i]);
         }
-        
-        list.add(matrix_sizes[matrix_sizes.length - 1]);
-        pq.add(min);
-        int sum = 0;
-        
-        while (list.size() > 1) {
-            int[] cur = pq.poll();
-            
-            int[] matrix = list.get(cur[0]);
-            sum += cur[1] * matrix[1];
-            
-            list.add(cur[0], new int[]{matrix[0], list.get(cur[0] + 1)[1]});
-            list.remove(cur[0] + 1);
-            list.remove(cur[0] + 1);
-            
-            min[0] = -1;
-            min[1] = Integer.MAX_VALUE;
-            
-            for (int i = 1; i < list.size(); i++) {
-                int multiValue = list.get(i - 1)[0] * list.get(i)[1];
                 
-                if (min[1] > multiValue) {
-                    min[0] = i - 1;
-                    min[1] = multiValue;
+        while (list.size() > 1) {
+            int minSum = Integer.MAX_VALUE;
+            int minMulti = Integer.MAX_VALUE;
+            int minIndex = 0;
+            
+            // 이전 값과 현재 값의 요소를 더한 값, 곱한 값이 최소가 되는 것 부터 먼저 계산
+            for (int i = 1; i < list.size(); i++) {
+                int[] pre = matrix_sizes[i - 1];
+                int[] cur = matrix_sizes[i];
+
+                int sum = pre[0] + pre[1] + cur[1];
+                int multi = pre[0] * pre[1] * cur[1];
+
+                if (minSum >= sum) {
+                    if (minSum > sum) {
+                        minSum = sum;
+                        minMulti = multi;
+                        minIndex = i;
+                    } else {
+                        if (minMulti > multi) {
+                            minMulti = multi;
+                            minIndex = i;
+                        }
+                    }
                 }
             }
             
-            pq.add(min);
+            int[] now = list.get(minIndex);
+            int[] left = list.get(minIndex -1);
+            
+            list.add(minIndex - 1, new int[]{left[0], now[1]});
+            list.remove(minIndex);
+            list.remove(minIndex);
+            
+            answer += minMulti;
         }
         
-        return answer = sum;
+        return answer;
     }
 }
