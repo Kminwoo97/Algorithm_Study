a4208476643d6c00bee7428f44f077d6b81f1193
BakChanGyu <103313634+BakChanGyu@users.noreply.github.com>
2023-12-19 00:56:36 +0900

    lv2 순위 검색(2)

--- programmers/chan/level2/순위 검색.java
+++ programmers/chan/level2/순위 검색.java
@@ -3,149 +3,61 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] info, String[] query) {
         int[] answer = new int[query.length];
-        int[] infoScores = new int[info.length]; 
+        Map<String, Integer> map = new HashMap<>();
+        List<String> list = new ArrayList<>();
         
-        // info를 코딩테스트 점수로 정렬
-        Arrays.sort(info, (e1, e2) -> {
-            int score1 = Integer.parseInt(e1.split(" ")[4]);
-            int score2 = Integer.parseInt(e2.split(" ")[4]);
-            
-            return score1 - score2;
-        });
-        
-        // info에서 점수 추출 
         for (int i = 0; i < info.length; i++) {
-            infoScores[i] = Integer.parseInt(info[i].split(" ")[4]);
-        }
-        
-        for (int i = 0; i < query.length; i++) {
-            String[] str = query[i].split(" ");
-            int score = Integer.parseInt(str[7]);
-                
-            // 이분탐색
-            int start = 0;
-            int end = info.length - 1;
-            int mid = 0;
+            String[] infos = info[i].split(" ");
             
-            while (start < end) {
-                mid = (start + end + 1) / 2;
-                
-                if (infoScores[mid] >= score) {
-                    end = mid - 1;
-                } else {
-                    start = mid + 1;
-                }
-            }
+            String[] language = {infos[0], "-"};
+            String[] position = {infos[1], "-"};
+            String[] role = {infos[2], "-"};
+            String[] food = {infos[3], "-"};
+            boolean[][] isVisited = new boolean[4][2];
             
-            String language = str[0];
-            String position = str[2];
-            String experience = str[4];
-            String food = str[6];
-            int count = 0;
+            // 2차원배열로 만들고 하기
             
-            for (int j = mid - 1; j < info.length; j++) {
-                String[] infos = info[j].split(" ");
-                boolean isMatched = true;
-                
-                if (infoScores[j] < score) {
-                    continue;
-                }
-                
-                if (language.equals("-")) {
-                    if (position.equals("-")) {
-                        if (experience.equals("-")) {
-                            if (food.equals("-")) {
-                                count++;
-                            } else if (food.equals(infos[3])) {
-                                count++;
-                            } else {
-                                continue;
-                            }
-                        } else if (experience.equals(infos[2])) {
-                            if (food.equals("-")) {
-                                count++;
-                            } else if (food.equals(infos[3])) {
-                                count++;
-                            } else {
-                                continue;
-                            }
-                        } else {
-                            continue;
-                        }
-                    } else if (position.equals(infos[1])) {
-                        if (experience.equals("-")) {
-                            if (food.equals("-")) {
-                                count++;
-                            } else if (food.equals(infos[3])) {
-                                count++;
-                            } else {
-                                continue;
-                            }
-                        } else if (experience.equals(infos[2])) {
-                            if (food.equals("-")) {
-                                count++;
-                            } else if (food.equals(infos[3])) {
-                                count++;
-                            } else {
-                                continue;
-                            }
-                        } else {
-                            continue;
-                        }
-                    } else {
-                        continue;
-                    }
-                } else if (language.equals(infos[0])) {
-                    if (position.equals("-")) {
-                        if (experience.equals("-")) {
-                            if (food.equals("-")) {
-                                count++;
-                            } else if (food.equals(infos[3])) {
-                                count++;
-                            } else {
-                                continue;
-                            }
-                        } else if (experience.equals(infos[2])) {
-                            if (food.equals("-")) {
-                                count++;
-                            } else if (food.equals(infos[3])) {
-                                count++;
-                            } else {
-                                continue;
-                            }
-                        } else {
-                            continue;
-                        }
-                    } else if (position.equals(infos[1])) {
-                        if (experience.equals("-")) {
-                            if (food.equals("-")) {
-                                count++;
-                            } else if (food.equals(infos[3])) {
-                                count++;
-                            } else {
-                                continue;
-                            }
-                        } else if (experience.equals(infos[2])) {
-                            if (food.equals("-")) {
-                                count++;
-                            } else if (food.equals(infos[3])) {
-                                count++;
-                            } else {
-                                continue;
-                            }
-                        } else {
-                            continue;
-                        }
-                    } else {
-                        continue;
-                    }
-                } else {
-                    continue;
-                }
-            }
-            answer[i] = count;
+            int score = Integer.parseInt(infos[4]);
+            String str = "";
+            
+            dfs(map, isVisited, language, position, role, food, score, str);
         }
         
         return answer;
+    }
+    
+    public void dfs(Map<String, Integer> map, boolean[][] isVisited, String[] language, String[] position, String[] role, String[] food, int score, String str) {
+        
+        // 2차원배열로 만들고 하기
+        for (int i = 0; i < 2; i++) {
+            if (isVisited[0][i] == false) {
+                isVisited[0][i] = true;
+                dfs(map, isVisited, language, position, role, food, score, str + language[i]);
+            }
+        }
+        
+        for (int i = 0; i < 2; i++) {
+            if (isVisited[1][i] == false) {
+                isVisited[1][i] = true;
+                dfs(map, isVisited, language, position, role, food, score, str + position[i]);
+            }
+        }
+        
+        for (int i = 0; i < 2; i++) {
+            if (isVisited[2][i] == false) {
+                isVisited[2][i] = true;
+                dfs(map, isVisited, language, position, role, food, score, str + role[i]);
+            }
+        }
+        
+        for (int i = 0; i < 2; i++) {
+            if (isVisited[3][i] == false) {
+                isVisited[3][i] = true;
+                dfs(map, isVisited, language, position, role, food, score, str + food[i]);
+                
+                map.put(str + food[i], score);
+                System.out.println(str + food[i] + " " + score);
+            }
+        }
     }
 }
