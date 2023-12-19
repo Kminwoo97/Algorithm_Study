5e557a2692a3171788d1572a59c09b9bd99276dd
BakChanGyu <103313634+BakChanGyu@users.noreply.github.com>
2023-12-19 15:25:11 +0900

    lv2 순위 검색(최종)

--- programmers/chan/level2/순위 검색.java
+++ programmers/chan/level2/순위 검색.java
@@ -1,10 +1,10 @@
 import java.util.*;
 
 class Solution {
+    static Map<String, List<Integer>> map = new HashMap<>();
+    
     public int[] solution(String[] info, String[] query) {
         int[] answer = new int[query.length];
-        Map<String, Integer> map = new HashMap<>();
-        List<String> list = new ArrayList<>();
         
         for (int i = 0; i < info.length; i++) {
             String[] infos = info[i].split(" ");
@@ -13,51 +13,75 @@ class Solution {
             String[] position = {infos[1], "-"};
             String[] role = {infos[2], "-"};
             String[] food = {infos[3], "-"};
-            boolean[][] isVisited = new boolean[4][2];
-            
-            // 2차원배열로 만들고 하기
-            
             int score = Integer.parseInt(infos[4]);
-            String str = "";
+            String[][] mapKeys = {language, position, role, food};
             
-            dfs(map, isVisited, language, position, role, food, score, str);
+            dfs(mapKeys, score, new ArrayList<>(), -1);
+        }
+        
+        for (String key : map.keySet()) {
+            Collections.sort(map.get(key));
+        }
+        
+        // map에 저장한 값 중 query의 score보다 큰 값 찾기
+        for (int i = 0; i < query.length; i++) {
+            String str = query[i].replaceAll(" and ", "");
+            String[] key = str.split(" ");
+            
+            int score = Integer.parseInt(key[1]);
+            
+            List<Integer> scoreList = map.getOrDefault(key[0], new ArrayList<>());
+            
+            if (scoreList.size() > 0) {
+                // 이분탐색
+                int left = 0;
+                int right = scoreList.size() - 1;
+                int mid = (left + right) / 2;
+                
+                while (left < right) {                    
+                    if (score > scoreList.get(mid)) {
+                        left = mid + 1;
+                    } else {
+                        right = mid - 1;
+                    }
+                    
+                    mid = (left + right) / 2;
+                }
+                
+                answer[i] = scoreList.size() - 1 - mid;
+                
+                if (scoreList.get(mid) >= score) {
+                    answer[i]++;
+                }
+            }
         }
         
         return answer;
     }
     
-    public void dfs(Map<String, Integer> map, boolean[][] isVisited, String[] language, String[] position, String[] role, String[] food, int score, String str) {
-        
-        // 2차원배열로 만들고 하기
-        for (int i = 0; i < 2; i++) {
-            if (isVisited[0][i] == false) {
-                isVisited[0][i] = true;
-                dfs(map, isVisited, language, position, role, food, score, str + language[i]);
+    public void dfs(String[][] mapKeys, int score, List<String> list, int nextIndex) {
+        if (list.size() == 4) {
+            String newStr = "";
+            
+            for (String str : list) {
+                newStr += str;
             }
+            
+            List<Integer> scoreList = map.getOrDefault(newStr, new ArrayList<>());
+            scoreList.add(score);
+            
+            map.put(newStr, scoreList);
         }
         
-        for (int i = 0; i < 2; i++) {
-            if (isVisited[1][i] == false) {
-                isVisited[1][i] = true;
-                dfs(map, isVisited, language, position, role, food, score, str + position[i]);
-            }
-        }
-        
-        for (int i = 0; i < 2; i++) {
-            if (isVisited[2][i] == false) {
-                isVisited[2][i] = true;
-                dfs(map, isVisited, language, position, role, food, score, str + role[i]);
-            }
-        }
-        
-        for (int i = 0; i < 2; i++) {
-            if (isVisited[3][i] == false) {
-                isVisited[3][i] = true;
-                dfs(map, isVisited, language, position, role, food, score, str + food[i]);
-                
-                map.put(str + food[i], score);
-                System.out.println(str + food[i] + " " + score);
-            }
+        for (int i = nextIndex + 1; i < 4; i++) {
+            List<String> sub1 = new ArrayList<>(list);
+            List<String> sub2 = new ArrayList<>(list);
+            
+            sub1.add(mapKeys[i][0]);
+            sub2.add(mapKeys[i][1]);
+            
+            dfs(mapKeys, score, sub1, i);
+            dfs(mapKeys, score, sub2, i);
         }
     }
 }
