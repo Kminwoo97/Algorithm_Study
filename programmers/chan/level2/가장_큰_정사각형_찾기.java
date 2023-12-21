a83c5738bf76eda8f5f2f4596af5bebf736bcee0
BakChanGyu <103313634+BakChanGyu@users.noreply.github.com>
2023-12-21 19:15:00 +0900

    lv2 가장 큰 정사각형 찾기(최종)

--- programmers/chan/level2/가장_큰_정사각형_찾기.java
+++ programmers/chan/level2/가장_큰_정사각형_찾기.java
@@ -1,52 +1,40 @@
+import java.util.*;
+
 class Solution
 {
-    static int max = 0;
-    
     public int solution(int [][]board)
     {
         int answer = 1234;
+        int max = 0;
+        int[][] dp = new int[board.length][board[0].length];
         
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
+        if (board.length < 2 || board[0].length < 2) {
+            for (int i = 0; i < board.length; i++) {
+                for (int j = 0; j < board[0].length; j++) {
+                    if (board[i][j] == 1) {
+                        max = 1;
+                    }
+                }
+            }
+        }
+        
+        for (int i = 1; i < board.length; i++) {
+            for (int j = 1; j < board[0].length; j++) {
                 if (board[i][j] == 1) {
-                    find(board, new int[]{i, j}, 1);
+                    // 왼, 위, 대각선 값 확인
+                    dp[i][j] = Math.min(dp[i][j - 1], Math.min(dp[i - 1][j - 1], dp[i - 1][j])) + 1;
+                    
+                    if (dp[i][j] == 1) {
+                        if (board[i][j - 1] == 1 && board[i - 1][j - 1] == 1 && board[i - 1][j] == 1) {
+                            dp[i][j] = 2;
+                        }
+                    }
+                    
+                    max = Math.max(max, dp[i][j]);
                 }
             }
         }
-
+        
         return answer = max * max;
-    }
-    
-    public void find(int[][] board, int[] cur, int count) {
-        max = Math.max(max, count);
-        
-        int[] next = {cur[0] + 1, cur[1] + 1};
-        
-        if (next[0] >= 0 && next[0] < board.length &&
-            next[1] >= 0 && next[1] < board[0].length &&
-            board[next[0]][next[1]] == 1) {
-            
-            boolean flag = true;
-            
-            for (int i = cur[0]; i < next[0]; i++) {
-                if (board[i][next[1]] != 1) {
-                    flag = false;
-                    break;
-                }
-            }
-            
-            for (int i = cur[1]; i < next[1]; i++) {
-                if (board[next[0]][i] != 1) {
-                    flag = false;
-                    break;
-                }
-            }
-            
-            if (flag) {
-                find(board, next, count + 1);
-            } else {
-                return;
-            }
-        }
     }
 }
