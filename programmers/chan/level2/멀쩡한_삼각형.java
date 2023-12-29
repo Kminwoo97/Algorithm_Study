f9ee59eeb8a4cb9d8df85ad4a8eac981fd5936fb
BakChanGyu <103313634+BakChanGyu@users.noreply.github.com>
2023-12-29 17:02:18 +0900

    lv2 멀쩡한 삼각형(최종)

--- programmers/chan/level2/멀쩡한_삼각형.java
+++ programmers/chan/level2/멀쩡한_삼각형.java
@@ -1,33 +1,21 @@
 class Solution {
     public long solution(int w, int h) {
-        long answer = w * h;
+        long answer = (long) w * h;
         
+        // 몇대 몇 비율인지 확인
         int gcd = getGCD(w, h);
         int row = w / gcd;
         int col = h / gcd;
         
         // 정사각형인 경우
         if (row == col) {
-            answer -= row;  
+            answer -= w; 
         } else {
             // 아닌경우
-            long sum = 0;
-            int remain = 0;
-                
-            if (row > col) {
-                remain = (row - 1) / col + 1;
-            } else {
-                remain = (col - 1) / row + 1;
-            }
+            long sum = row * col;
             
-            if (row == 1 || col == 1) {
-                sum += remain;
-            } else {
-                sum += remain * 2;
-                sum += (col - (remain - 1) * 2) * (row - 2);
-            }
-            
-            answer -= sum * (w / row);
+            sum -= (long) ((row - 1) * (col - 1));
+            answer -= sum * gcd;
         }
         
         return answer;
