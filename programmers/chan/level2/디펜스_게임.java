f3b1a331785fb6f7b44d8fae1425fe06c1c8bd59
BakChanGyu <103313634+BakChanGyu@users.noreply.github.com>
2024-01-02 17:53:40 +0900

    lv2 디펜스 게임(최종)

--- programmers/chan/level2/디펜스_게임.java
+++ programmers/chan/level2/디펜스_게임.java
@@ -3,15 +3,21 @@ import java.util.*;
 class Solution {
     public int solution(int n, int k, int[] enemy) {
         int answer = enemy.length;
+        PriorityQueue<Integer> pq = new PriorityQueue<>((e1, e2) -> e2 - e1);
+        int remain = n;
         
-        Arrays.sort(enemy);
-        
-        for (int i = enemy.length - 1; i > k; i--) {
-            n -= enemy[i];
+        for (int i = 0; i < enemy.length; i++) {
+            remain -= enemy[i];
+            pq.add(enemy[i]);
             
-            if (n < 0) {
-                answer = i;
-                break;
+            if (remain < 0) {
+                if (!pq.isEmpty() && k > 0) {
+                    remain += pq.poll();
+                    k--;
+                } else {
+                    answer = i;
+                    break;
+                }
             }
         }
         
