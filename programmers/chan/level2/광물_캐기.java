898f0dbaa2c7939e39f044a3c2e105cd97600c78
BakChanGyu <103313634+BakChanGyu@users.noreply.github.com>
2024-01-04 20:23:10 +0900

    lv2 광물 캐기(최종)

--- programmers/chan/level2/광물_캐기.java
+++ programmers/chan/level2/광물_캐기.java
@@ -3,7 +3,10 @@ import java.util.*;
 class Solution {
     public int solution(int[] picks, String[] minerals) {
         int answer = 0;
+        int pickaxe = picks[0] + picks[1] + picks[2];
         int[] pick = new int[3];
+        
+        List<int[]> list = new ArrayList<>();
         
         for (int i = 0; i < minerals.length; i++) {
             if (minerals[i].equals("diamond")) {
@@ -20,37 +23,33 @@ class Solution {
                 pick[2] += 1;
             }
             
-            if (i % 5 == 4) {
-                int min = Integer.MAX_VALUE;
-                int minIndex = -1;
-                
-                for (int j = 0; j < 3; j++) {
-                    if (picks[j] != 0 && min > pick[j]) {
-                        min = pick[j];
-                        minIndex = j;
-                    }
-                }
-                
-                answer += min;
-                picks[minIndex]--;
+            if (i % 5 == 4 && list.size() < pickaxe) {
+                list.add(new int[]{pick[0], pick[1], pick[2]});
                 pick[0] = 0;
                 pick[1] = 0;
                 pick[2] = 0;
             }
         }
         
-        if (picks[0] != 0 || picks[1] != 0 || picks[2] != 0) {
-            int min = Integer.MAX_VALUE;
-            int minIndex = -1;
-
-            for (int j = 0; j < 3; j++) {
-                if (picks[j] != 0 && min > pick[j]) {
-                    min = pick[j];
-                    minIndex = j;
-                }
+        if ((minerals.length - 1) / 5 + 1 <= pickaxe) {
+            list.add(new int[]{pick[0], pick[1], pick[2]});
+        }
+        
+        Collections.sort(list, (e1, e2) -> e2[2] - e1[2]);
+        
+        for (int i = 0; i < list.size(); i++) {
+            if (picks[0] > 0) {
+                answer += list.get(i)[0];
+                picks[0]--;
+            } else if (picks[1] > 0) {
+                answer += list.get(i)[1];
+                picks[1]--;
+            } else if (picks[2] > 0) {
+                answer += list.get(i)[2];
+                picks[2]--;
+            } else {
+                break;
             }
-
-            answer += min;
         }
         
         return answer;
