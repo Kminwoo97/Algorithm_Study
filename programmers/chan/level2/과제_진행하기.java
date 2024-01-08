271d1ed7bc5ee7b373fdd946bc87de16ab8dfbe2
BakChanGyu <103313634+BakChanGyu@users.noreply.github.com>
2024-01-08 16:26:32 +0900

    lv2 과제 진행하기(2)

--- programmers/chan/level2/과제_진행하기.java
+++ programmers/chan/level2/과제_진행하기.java
@@ -9,49 +9,53 @@ class Solution {
         Arrays.sort(plans, (e1, e2) -> e1[1].compareTo(e2[1]));
         
         for (int i = 0; i < plans.length; i++) {
-            if (!stack.isEmpty()) {
+            if (stack.isEmpty()) {
+                stack.push(new Assignment(plans[i][0], 
+                                          convertToInt(plans[i][1]), 
+                                          Integer.parseInt(plans[i][2])));
+            } else {
                 Assignment pre = stack.pop();
+                int timediff = convertToInt(plans[i][1]) - pre.start;
                 
-                int remain = pre.start + pre.playTime - convertToInt(plans[i][1]);
-                
-                if (remain > 0) {
-                    pre.start = convertToInt(plans[i][1]) + Integer.parseInt(plans[i][2]);
-                    pre.playTime = remain;
-                    stack.push(pre);
-                } else if (remain < 0) {
+                // 이전 작업 끝과 동시에 새 작업 시작하는 경우
+                if (timediff == pre.playTime) {
                     ans.add(pre.name);
-                    int remain2 = -remain;
+                } else if (timediff < pre.playTime) {
+                    // 이전 작업이 끝나기 전에 새 작업 시작하는 경우
+                    pre.playTime -= timediff;
+                    stack.push(pre);
+                } else {
+                    // 이전 작업 끝난 후 새 작업 시작하는 경우
+                    ans.add(pre.name);
+                    int remainTime = timediff - pre.playTime;
                     
-                    while (remain2 > 0) {
-                        stack.pop();
-                        
+                    while (remainTime > 0) {
+                        // 남은 시간동안 스택에 있는 작업 처리
                         if (!stack.isEmpty()) {
                             Assignment pre2 = stack.pop();
                             
-                            if (remain2 >= pre2.playTime) {
-                                remain2 -= pre2.playTime;
-                                ans.add(pre2.name);
-                            } else {
-                                pre2.playTime = pre2.playTime - remain2;
+                            int timediff2 = pre2.playTime - remainTime;
+                            
+                            // 남은 작업 처리 다 못하는 경우
+                            if (timediff2 > 0) {
+                                pre2.playTime -= remainTime;
                                 stack.push(pre2);
+                                remainTime = 0;
+                            } else {
+                                remainTime -= pre2.playTime;
                             }
                         }
                     }
                 }
                 
                 stack.push(new Assignment(plans[i][0], 
-                                              convertToInt(plans[i][1]), 
-                                              Integer.parseInt(plans[i][2])));
-            } else {
-                stack.push(
-                    new Assignment(plans[i][0], 
-                                   convertToInt(plans[i][1]), 
-                                   Integer.parseInt(plans[i][2])));
+                                          convertToInt(plans[i][1]), 
+                                          Integer.parseInt(plans[i][2])));
             }
-            
-            while (!stack.isEmpty()) {
-                ans.add(stack.pop().name);
-            }
+        }
+        
+        while (!stack.isEmpty()) {
+            ans.add(stack.pop().name);
         }
         
         for (int i = 0; i < ans.size(); i++) {
