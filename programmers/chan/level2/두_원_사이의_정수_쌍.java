97b0cff6c41685743458f530286c7014d1282bcc
BakChanGyu <103313634+BakChanGyu@users.noreply.github.com>
2024-01-12 20:03:20 +0900

    lv2 두 원 사이의 정수 쌍(최종)

--- programmers/chan/level2/두_원_사이의_정수_쌍.java
+++ programmers/chan/level2/두_원_사이의_정수_쌍.java
@@ -2,22 +2,13 @@ class Solution {
     public long solution(int r1, int r2) {
         long answer = 0;
         
-        for (int i = -r2; i <= r2; i++) {
+        for (int i = 1; i <= r2; i++) {
             double x1 = Math.sqrt(Math.pow(r1, 2) - Math.pow(i, 2));
             double x2 = Math.sqrt(Math.pow(r2, 2) - Math.pow(i, 2));
             
-            if ((int) x2 == x2) { 
-                answer++;
-            }
-            
-            if ((int) x1 == x1) {
-                answer++;
-            }
-            
-            answer += ((int) x2 * 2 + 1) - (Math.ceil((int) x1) * 2 + 1);
-            System.out.println(i + " " + answer);
+            answer += (int) x2 - (int) Math.ceil(x1) + 1;
         }
         
-        return answer;
+        return answer *= 4;
     }
 }
