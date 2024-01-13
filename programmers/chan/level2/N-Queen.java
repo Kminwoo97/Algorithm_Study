a9419a056b88aa5918a0321e7b10ee4753f33a6e
BakChanGyu <103313634+BakChanGyu@users.noreply.github.com>
2024-01-13 22:19:19 +0900

    lv2 N-Queen(최종)

--- programmers/chan/level2/N-Queen.java
+++ programmers/chan/level2/N-Queen.java
@@ -5,15 +5,18 @@ class Solution {
         int answer = 0;
         
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                find(n, new int[]{i, j}, new boolean[n][n], 1);
-            }
+            find(n, new int[]{0, i}, new boolean[n][n], 1);
         }
         
         return answer = ans;
     }
     
     public void find(int n, int[] start, boolean[][] isVisited, int count) {
+        
+        if (start[0] == n - 1 && count == n) {
+            ans++;
+            return;
+        }
         
         for (int i = 0; i < n; i++) {
             if (start[1] + i < n) {
@@ -33,18 +36,19 @@ class Solution {
             }
         }
         
-        if (n == count) {
-            ans++;
-            return;
-        }
-        
-        for (int i = start[0]; i < n; i++) {
-            for (int j = start[1]; j < n; j++) {
-                if (isVisited[i][j] == false) {
-                    isVisited[i][j] = true;
-                    System.out.println(i + " " + j + " " + count + 1);
-                    find(n, new int[]{i, j}, isVisited, count + 1);
+        for (int i = 0; i < n; i++) {
+            if (isVisited[start[0] + 1][i] == false) {
+                boolean[][] sub = new boolean[n][n];
+                
+                for (int j = 0; j < n; j++) {
+                    for (int k = 0; k < n; k++) {
+                        if (isVisited[j][k] == true) {
+                            sub[j][k] = true;
+                        }
+                    }
                 }
+                
+                find(n, new int[]{start[0] + 1, i}, sub, count + 1);
             }
         }
     }
