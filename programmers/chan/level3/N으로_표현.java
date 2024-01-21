fa577efa0e4dfda7a91133db3272a43db665884b
BakChanGyu <103313634+BakChanGyu@users.noreply.github.com>
2024-01-21 17:08:56 +0900

    lv3 N으로 표현(최종)

--- programmers/chan/level3/N으로_표현.java
+++ programmers/chan/level3/N으로_표현.java
@@ -1,28 +1,70 @@
+import java.util.*;
+
 class Solution {
-    static int min = Integer.MAX_VALUE;
-    
     public int solution(int N, int number) {
-        int answer = 0;
+        int answer = -1;
+        List<Set<Integer>> dp = new ArrayList<>();
         
-        dfs(N, number, 0, 0);
-        
-        return answer = min;
-    }
-    
-    public void dfs(int N, int number, int depth, int sum) {
-        if (depth > 8) {
-            min = -1;
-            return;
+        for (int i = 0; i < 9; i++) {
+            dp.add(new HashSet<>());
         }
         
-        if (number == sum) {
-            min = Math.min(min, sum);
-            return;
+        dp.get(1).add(N);
+        
+        if (N == number) {
+            return answer = 1;
+        } 
+        
+        for (int i = 2; i < 9; i++) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(String.valueOf(N).repeat(i));
+            int value = Integer.parseInt(sb.toString());
+
+            if (value == number) {
+                answer = i;
+                break;
+            } else {
+                dp.get(i).add(value);
+            }
+            
+            boolean flag = false;
+            
+            for (int j = 1; j < i; j++) {
+                Set<Integer> pre1 = dp.get(i - j);
+                Set<Integer> pre2 = dp.get(j);
+                
+                for (int el1 : pre1) {
+                    for (int el2 : pre2) {
+                        int plus = el1 + el2;
+                        int minus = el1 - el2; 
+                        int multi = el1 * el2;
+                        int divide = el1 / el2;
+
+                        if (plus == number || minus == number || multi == number || divide == number) {
+                            flag = true;
+                            break;
+                        }
+                        
+                        if (plus != 0) dp.get(i).add(plus);
+                        if (minus != 0) dp.get(i).add(minus);
+                        if (multi != 0) dp.get(i).add(multi);
+                        if (divide != 0) dp.get(i).add(divide);
+                    }
+                    
+                    if (flag) break;
+                    
+                }
+                
+                if (flag) break;
+                
+            }
+            
+            if (flag) {
+                answer = i;
+                break;
+            }
         }
         
-        dfs(N, number, depth + 1, sum + 5);
-        dfs(N, number, depth + 1, sum - 5);
-        dfs(N, number, depth + 1, sum * 5);
-        dfs(N, number, depth + 1, sum / 5);
+        return answer;
     }
 }
