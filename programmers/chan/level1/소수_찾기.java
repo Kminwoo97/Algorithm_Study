f0c962d1183e60fe6da32045dc7112251bdf7c9e
BakChanGyu <103313634+BakChanGyu@users.noreply.github.com>
2024-01-25 16:55:44 +0900

    lv1 소수 찾기

--- programmers/chan/level1/소수_찾기.java
+++ programmers/chan/level1/소수_찾기.java
@@ -1,25 +1,17 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        
-        boolean[] prime = new boolean[n + 1];
-        
-        prime[0] = true;
-        prime[1] = true;
+        boolean[] isVisited = new boolean[n + 1];
         
         for (int i = 2; i <= n; i++) {
-            if (prime[i] == false) {
-                for (int j = i + i; j <= n; j += i) {
-                    prime[j] = true;
+            if (isVisited[i] == false) {
+                answer++;
+                
+                for (int j = 1; i * j <= n; j++) {
+                    isVisited[i * j] = true;
                 }
             }
-        }
-        
-        for (int i = 1; i <= n; i++) {
-            if (prime[i] == false) {
-                answer++;
-            }
-        }
+        }   
         
         return answer;
     }
