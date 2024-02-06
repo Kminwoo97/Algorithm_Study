44f343256a961a6211b398e4ca688c05790e0d38
BakChanGyu <103313634+BakChanGyu@users.noreply.github.com>
2024-02-06 16:35:32 +0900

    lv2 올바른 괄호

--- programmers/chan/level2/올바른_괄호.java
+++ programmers/chan/level2/올바른_괄호.java
@@ -1,23 +1,28 @@
 import java.util.*;
 
-public class 올바른_괄호 {
+class Solution {
     boolean solution(String s) {
         boolean answer = true;
         Stack<Character> stack = new Stack<>();
-
+        
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '(') {
-                stack.push(s.charAt(i));
+                stack.push('(');
             } else {
-                if (!stack.isEmpty()) {
-                    stack.pop();
-                } else {
+                if (stack.isEmpty()) {
                     answer = false;
                     break;
+                } else {
+                    char pre = stack.pop();
+                    
+                    if (pre != '(') {
+                        answer = false;
+                        break;
+                    }
                 }
             }
         }
-
+        
         if (!stack.isEmpty()) {
             answer = false;
         }
