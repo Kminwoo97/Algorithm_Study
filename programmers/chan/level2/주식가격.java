c4ce9df85b1a97f5eb4dc1fa0aadbb0e917c94c0
BakChanGyu <103313634+BakChanGyu@users.noreply.github.com>
2024-02-16 15:38:10 +0900

    lv2 주식가격

--- programmers/chan/level2/주식가격.java
+++ programmers/chan/level2/주식가격.java
@@ -1,25 +1,16 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
         
-        // prices의 각 요소를 비교한다.
-        // 1. prices[i]에서 인덱스를 증가해가면서 뒤의 값과 비교한다.
-        // 2. prices[i]보다 작다면 해당 인덱스 값 - i 하면 가격이 떨어지지않은 기간이 나온다.
-        // 3. 끝까지 진행했을때 isDowned가 false라면 prices.length - 1 - i 하여 가격이 떨어지지않은 기간을 구할 수 있다.
-        
         for (int i = 0; i < prices.length; i++) {
-            boolean isDowned = false;
-            
-            for (int j = i; j < prices.length; j++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                answer[i]++;
+                
                 if (prices[i] > prices[j]) {
-                    answer[i] = j - i;
-                    isDowned = true;
                     break;
                 }
-            }
-            
-            if (isDowned == false) {
-                answer[i] = prices.length - 1 - i;
             }
         }
         
