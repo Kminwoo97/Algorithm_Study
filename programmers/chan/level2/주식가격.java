77190d271008d439eb8e97444f5f64db604443ee
BakChanGyu <103313634+BakChanGyu@users.noreply.github.com>
2024-02-16 15:54:34 +0900

    lv2 주식가격

--- programmers/chan/level2/주식가격.java
+++ programmers/chan/level2/주식가격.java
@@ -17,3 +17,33 @@ class Solution {
         return answer;
     }
 }
+
+// stack 사용한 코드
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        Stack<int[]> stack = new Stack<>();
+        int len = prices.length - 1;
+        
+        for (int i = 0; i <= len; i++) {
+            while (!stack.isEmpty() && stack.peek()[1] > prices[i]) {
+                int[] pre = stack.pop();
+                
+                answer[pre[0]] = i - pre[0];
+            }
+        
+            stack.push(new int[]{i, prices[i]});
+        }
+        
+        while (!stack.isEmpty()) {
+            int[] cur = stack.pop();
+            
+            answer[cur[0]] = len - cur[0];
+        }
+        
+        return answer;
+    }
+}
