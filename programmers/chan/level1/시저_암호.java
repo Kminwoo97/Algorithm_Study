a4fda440145a02f3268b2216c374a4df3dcdde3c
BakChanGyu <103313634+BakChanGyu@users.noreply.github.com>
2024-03-02 18:10:48 +0900

    lv1 시저 암호(2)

--- programmers/chan/level1/시저_암호.java
+++ programmers/chan/level1/시저_암호.java
@@ -2,27 +2,20 @@ class Solution {
     public String solution(String s, int n) {
         String answer = "";
         StringBuilder sb = new StringBuilder();
-
+        
         for (int i = 0; i < s.length(); i++) {
-            char alpha = (char) (s.charAt(i) + n);
-
-            if (s.charAt(i) != ' ') {
-                if (s.charAt(i) <= 90) {
-                    if (alpha > 90) {
-                        alpha -= 26;
-                    }
+            char ch = s.charAt(i);
+            if (ch != ' ') {
+                if (ch < 97) {
+                    sb.append((char) ((ch + n) % 65 % 26 + 65));
                 } else {
-                    if (alpha > 122) {
-                        alpha -= 26;
-                    }
+                    sb.append((char) ((ch + n) % 97 % 26 + 97));
                 }
-
-                sb.append(alpha);
             } else {
-                sb.append(s.charAt(i));
+                sb.append(" ");
             }
         }
-
+        
         return answer = sb.toString();
     }
 }
